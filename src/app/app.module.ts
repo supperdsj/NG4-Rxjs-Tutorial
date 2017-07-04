94e99e0fcc94b6bce7d27dc5ba3060ba8e52d14e
dongsj <232171773@qq.com>
2017-07-04 18:22:59 +0800

    s1 finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserEventExperimentsComponent } from './browser-event-experiments/browser-event-experiments.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BrowserEventExperimentsComponent
   ],
   imports: [
     BrowserModule
